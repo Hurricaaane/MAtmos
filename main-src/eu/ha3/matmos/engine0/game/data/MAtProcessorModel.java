926b02cbbee309694a2c61196757bc2c5d1c1878
Hurricaaane <git@ha3.eu>
2013-12-04 14:59:30 +0100

    Removed license at the top of every file, as all files in this repository fall under the same license (the code is still under WTFPLv2 though)

--- main-src/eu/ha3/matmos/engine0/game/data/MAtProcessorModel.java
+++ main-src/eu/ha3/matmos/engine0/game/data/MAtProcessorModel.java
@@ -4,21 +4,7 @@ import eu.ha3.matmos.engine0.conv.ProcessorModel;
 import eu.ha3.matmos.engine0.core.implem.StringData;
 import eu.ha3.matmos.engine0.game.system.MAtMod;
 
-/*
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-                    Version 2, December 2004 
-
- Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
-
- Everyone is permitted to copy and distribute verbatim or modified 
- copies of this license document, and changing it is allowed as long 
- as the name is changed. 
-
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
-
-  0. You just DO WHAT THE FUCK YOU WANT TO. 
-*/
+/* x-placeholder */
 
 public abstract class MAtProcessorModel extends ProcessorModel
 {
--- main-src/eu/ha3/matmos/engine0/game/system/MAtSoundManagerMaster.java
+++ main-src/eu/ha3/matmos/engine0/game/system/MAtSoundManagerMaster.java
@@ -12,21 +12,7 @@ import paulscode.sound.SoundSystemConfig;
 import eu.ha3.matmos.engine0.conv.CustomVolume;
 import eu.ha3.matmos.engine0.conv.ReplicableSoundRelay;
 
-/*
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-                    Version 2, December 2004 
-
- Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
-
- Everyone is permitted to copy and distribute verbatim or modified 
- copies of this license document, and changing it is allowed as long 
- as the name is changed. 
-
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
-
-  0. You just DO WHAT THE FUCK YOU WANT TO. 
-*/
+/* x-placeholder */
 
 public class MAtSoundManagerMaster implements ReplicableSoundRelay, CustomVolume
 {
