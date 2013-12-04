4625fa3cdf28384f59674367d24348ac8e2b2297
Hurricaaane <git@ha3.eu>
2013-12-04 14:59:30 +0100

    Removed license at the top of every file, as all files in this repository fall under the same license (the code is still under WTFPLv2 though)
    Conflicts:
    	main-src/eu/ha3/matmos/engine/implem/UtilityLoader.java
    	main-src/eu/ha3/matmos/engine0/core/implem/ConditionType.java
    	main-src/eu/ha3/matmos/engine0/core/parsers/JasonExpansions_Engine1.java
    	main-src/eu/ha3/matmos/engine0/core/serializers/JasonExpansions_Engine1_Serializer.java

--- main-src/eu/ha3/matmos/game/mod/mod_MAtmos.java
+++ main-src/eu/ha3/matmos/game/mod/mod_MAtmos.java
@@ -3,21 +3,7 @@ package eu.ha3.matmos.game.mod;
 import eu.ha3.matmos.game.system.MAtMod;
 import eu.ha3.mc.haddon.litemod.HaddonBridgeModLoader;
 
-/*
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-                    Version 2, December 2004 
-
- Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
-
- Everyone is permitted to copy and distribute verbatim or modified 
- copies of this license document, and changing it is allowed as long 
- as the name is changed. 
-
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
-
-  0. You just DO WHAT THE FUCK YOU WANT TO. 
-*/
+/* x-placeholder */
 
 public class mod_MAtmos extends HaddonBridgeModLoader
 {
