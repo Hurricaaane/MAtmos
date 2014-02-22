92396a3b5e6e6320171cb1649d91fab62fde22aa
Hurricaaane <git@ha3.eu>
2014-02-22 16:18:42 +0100

    Removed tobedeprecated and added summary of missing features

---
+++ main-src/eu/ha3/matmos/game/tobedeprecated/_PROCESSOR_MISSING.java
@@ -0,0 +1,36 @@
+package eu.ha3.matmos.game.tobedeprecated;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.World;
+import eu.ha3.matmos.engine0.core.interfaces.Data;
+import eu.ha3.matmos.game.data.abstractions.module.ModuleProcessor;
+import eu.ha3.matmos.game.system.MAtmosUtility;
+
+/*
+--filenotes-placeholder
+*/
+
+public class _PROCESSOR_MISSING extends ModuleProcessor
+{
+	public _PROCESSOR_MISSING(Data data, String name)
+	{
+		super(data, "_PROCESSOR_MISSING");
+	}
+	
+	@Override
+	protected void doProcess()
+	{
+		Minecraft mc = Minecraft.getMinecraft();
+		World w = mc.theWorld;
+		int x = MAtmosUtility.getPlayerX();
+		int y = MAtmosUtility.getPlayerY();
+		int z = MAtmosUtility.getPlayerZ();
+		
+		String deprecatedBiomeId = "-1";
+		
+		setValueLegacyIntIndexes(26, w.canBlockSeeTheSky(x, y, z) && !(w.getTopSolidOrLiquidBlock(x, z) > y) ? 1 : 0);
+		setValueLegacyIntIndexes(59, 0);
+		setValueLegacyIntIndexes(5, w.provider.dimensionId);
+		setValueLegacyIntIndexes(29, deprecatedBiomeId);
+	}
+}
