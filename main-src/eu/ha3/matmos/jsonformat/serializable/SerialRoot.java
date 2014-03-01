1a7c355cfbe6bf5793c4504ac3c1ec84a2f1c37b
Hurricaaane <git@ha3.eu>
2014-03-01 17:21:07 +0100

    Changed serialization field order

--- main-src/eu/ha3/matmos/jsonformat/serializable/SerialRoot.java
+++ main-src/eu/ha3/matmos/jsonformat/serializable/SerialRoot.java
@@ -9,9 +9,9 @@ import java.util.Map;
 
 public class SerialRoot
 {
-	public Map<String, SerialDynamic> dynamic = new LinkedHashMap<String, SerialDynamic>();
 	public Map<String, SerialEvent> event = new LinkedHashMap<String, SerialEvent>();
 	public Map<String, SerialList> list = new LinkedHashMap<String, SerialList>();
+	public Map<String, SerialDynamic> dynamic = new LinkedHashMap<String, SerialDynamic>();
 	public Map<String, SerialCondition> condition = new LinkedHashMap<String, SerialCondition>();
 	public Map<String, SerialSet> set = new LinkedHashMap<String, SerialSet>();
 	public Map<String, SerialMachine> machine = new LinkedHashMap<String, SerialMachine>();
